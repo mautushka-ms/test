6bdc684786d71cacf08199e4546d9263af8d043e
Maryna Autushka <m.autushka@melsoft-games.com>
2021-10-29 10:47:37 +0300

    sum workflow test

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-if __name__ == "main":
-    print "HELLO WORLD!"
-    print "1"
-    print "2"
-    print "3"
+
+import sys
+
+
+def main():
+    a = sys.argv[1]
+    b = sys.argv[2]
+    print('Sum is ..', int(a) + int(b))
+
+
+if __name__ == '__main__':
+    main()
+
+
