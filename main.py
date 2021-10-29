668bc95d75615be2b7cc57e62bbc503e9e4a8770
Maryna Autushka <m.autushka@melsoft-games.com>
2021-10-29 12:02:12 +0300

    sum workflow test

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import sys
 
 
 def main():
+    if len(sys.argv) > 2:
+        print("Invalid number of arguments passed!")
+        exit(2)
     a = sys.argv[1]
     b = sys.argv[2]
     print('B is ..', b)
